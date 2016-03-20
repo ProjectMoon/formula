1336d7d2b03b3155a529ba406b4963a5feaf8f1f
jeff <ProjectMoon@users.noreply.github.com>
2016-03-20 23:53:29 +0000

    login system, better car links

--- app.rb
+++ app.rb
@@ -21,8 +21,34 @@ module FormulaE::Web
     helpers Shield::Helpers
 
     helpers do
+      def has_errors
+        !error_message.nil? && !error_message.empty?
+      end
+
+      def error_message_html
+        html = "<div class='alert alert-danger' role='alert'>"
+        html += " <span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>"
+        html += "<span class='sr-only'>Error:</span>"
+        html += error_message
+        html += "</div>"
+        session[:error_message] = nil # clear it so it's not shown more than once
+        html
+      end
+
+      def error_message
+        session[:error_message]
+      end
+
+      def error_message=(msg)
+        session[:error_message] = msg
+      end
+
       def user
         authenticated(User)
+      end
+
+      def logged_in
+        authenticated(User) != nil
       end
 
       def username
@@ -47,12 +73,33 @@ module FormulaE::Web
       erb :cars, :locals => { racers: Racer.all }
     end
 
+    get '/login' do
+      erb :login
+    end
+
+    post '/login' do
+      puts "hello"
+      puts params
+      if login(User, params[:email], params[:password])
+        remember(user) if params[:remember]
+        redirect('/')
+      else
+        session[:error_message] = 'Invalid username or password.'
+        redirect('/login')
+      end
+    end
+
     get '/secure/add_race' do
       erb :add_race
     end
 
     post '/secure/add_race' do
       # TODO add the race.
+    end
+
+    get '/logout' do
+      logout(User)
+      redirect '/'
     end
   end
 end
